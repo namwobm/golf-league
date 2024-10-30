39d76a2e2e9186a70d901458a71a4a30653b49fa
Mark Bowman <markbowman@macbookpro.home>
2024-10-30 12:30:02 -0400

    Fix file structure and imports

--- src/components/GolfLeagueApp.js
+++ src/components/GolfLeagueApp.js
@@ -1,9 +1,9 @@
 'use client';
 
 import React, { useState } from 'react';
-import { Card, CardHeader, CardTitle, CardContent } from './components/ui/card';
-import { Button } from './components/ui/button';
-import { Input } from './components/ui/input';
+import { Card, CardHeader, CardTitle, CardContent } from './ui/card';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
 
 const GolfLeagueApp = () => {
   const [players, setPlayers] = useState([]);
